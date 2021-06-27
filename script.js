5e7f53ee9187777f325f6f1d1cf62a10545177a2
Dan <njskater1490@gmail.com>
2021-06-27 11:02:20 -0400

    removed
    removed stuff

--- script.js
+++ script.js
@@ -1,29 +1,3 @@
 $(document).ready(function() {
-    setInterval ('cursorAnimation()', 600);
+
 });
-
-var showText = function (target, message, index, interval) {
-    if (index < message.length) {
-        $(target).append(message[index++]);
-        //begin edit------------------------------|
-        if (message[index] == '\n') {
-            $(target).append("<br />");
-        }
-        //end edit--------------------------------|      
-        setTimeout(function () {
-            showText(target, message, index, interval);
-        }, interval);
-    }
-}
-
-$(function () {
-    showText("#msg", "IS ANYONE THERE?\n>                 OH -\n>                 HI!", 0, 50);
-});
-
-function cursorAnimation() {
-    $('#cursor').animate({
-        opacity: 0
-    }, 'fast', 'swing').animate({
-        opacity: 1
-    }, 'fast', 'swing');
-}
